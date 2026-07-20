2ba3cc8471dad0b4032bde95b0b4589fb0cfc912
zakitizen <ikazimamumamu@gmail.com>
2026-07-20 18:47:43 +0700

    Setup vercel deployment

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Mengarahkan request ke file index.php bawaan Laravel yang ada di folder public
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,28 @@
 <?php
 
-use App\Http\Middleware\PewartaMiddleware;
-use App\Http\Middleware\RedaksiMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Tambahkan pengecekan ini di bagian paling atas untuk Vercel
+if (isset($_SERVER['VERCEL_BUILD_ID'])) {
+    $appConfig = [
+        'view.compiled' => '/tmp/storage/framework/views',
+        'cache.stores.file.path' => '/tmp/storage/framework/cache/data',
+    ];
+    foreach ($appConfig as $key => $value) {
+        config([$key => $value]);
+    }
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'pewarta' => PewartaMiddleware::class,
-            'redaksi' => RedaksiMiddleware::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
